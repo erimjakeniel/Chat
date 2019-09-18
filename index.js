5ca0b47efe1c99f326d4dad4bf41df51c8c37c7e
1styrGroupC <1styrGroupC@pn.local>
2019-09-18 17:19:27 +0800

    updated

--- index.js
+++ index.js
@@ -2,27 +2,53 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var clients =[];
 
-app.get('/', function(req, res){
+
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.sockets.on('connection', function(socket){
-  socket.on('username', function(username) {
-    socket.username = username;
-    io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
-});
-socket.on('disconnect', function(username) {
-  io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
-})
 
-  socket.on('chat message', function(msg){
-    io.emit('chat message','<strong>' + socket.username + '<strong>:' +  msg);
+io.sockets.on('connection', function (socket) {
+  socket.on('username', function (username) {
+    socket.username = username;
+    io.emit('is_online', '🔵 <i>' + socket.username + ' join the group. ..</i>');
+    clients.push(socket);
+		users = getUserList();
+		io.emit("userList", users);
+
+
+  });
+  socket.on('disconnect', function (username) {
+      clients = clients.filter(function(e){  
+        return e!==socket;
+      });
+      updateUsernames();
+      io.emit('is_online', '🔴 <i>' + socket.username + ' left the group...</i>');
+
+  })
+
+
+  socket.on('chat message', function (msg) {
+    io.emit('chat message', '<strong>' + socket.username + '<strong>: ' + msg);
   });
 
+  function updateUsernames(){
+		users = getUserList();
+		io.emit("userList", users);
+	}
+
+  function getUserList(){
+		var ret = [];
+		for(var i=0;i<clients.length;i++){
+			ret.push(clients[i].username);
+		}
+		return ret;
+	}
 });
 
 
-var sever = http.listen(port, function(){
+var sever = http.listen(port, function () {
   console.log('listening on *:' + port);
 });
